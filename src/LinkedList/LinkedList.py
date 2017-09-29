65d069b3971d5e317c0aaca417c1d25301ac5360
dbairaktaris1 <dbairaktaris1@luc.edu>
2017-09-29 14:39:00 -0500

    Implementation of Single-Linked List

---
+++ src/LinkedList/LinkedList.py
@@ -0,0 +1,109 @@
+
+
+class Node:
+
+    def __init__(self, value=None,next=None):
+        self.value = value
+        self.next = next
+
+    def setValue(self, value):
+        self.value = value
+
+    def setNext(self, nextNode):
+        self.next = nextNode
+
+    def getValue(self):
+        return self.value
+
+    def getNext(self):
+        return self.next
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.root = None
+        self.length = 0
+        self.last = None
+
+    def __add__(self, value):
+        newNode = Node(value)
+
+        if self.length == 0:
+            self.root = newNode
+            self.last = self.root
+        else:
+            self.last.setNext(newNode)
+            self.last = newNode
+
+        self.length += 1
+
+    def __str__(self):
+        currentNode = self.root
+        l = []
+
+        while  currentNode!= None:
+            l.append(currentNode.getValue())
+            currentNode = currentNode.getNext()
+            
+        return str(l)
+
+    def pop(self):
+        self.root = self.root.next
+        self.length -= 1
+
+    def removeValue(self,value):
+        previousNode = None
+        currentNode = self.root
+        while currentNode != None:
+            if currentNode.getValue() == value:
+                if previousNode == None:
+                    self.pop()
+                else:
+                    previousNode.setNext(currentNode.getNext())
+
+                self.length -= 1
+                break
+            else:
+                placeHolder = currentNode
+                currentNode = currentNode.getNext()
+                previousNode = placeHolder
+
+    def removeIndex(self,index):
+        if(index >= self.length or index < -self.length):
+            return
+        index = index % self.length
+        index = abs(index)
+        if(index == 0 or index == 3):
+            self.pop()
+            return
+
+        previousNode = None
+        currentNode = self.root
+        i = 0
+        while i < index:
+            placeholder = currentNode
+            currentNode = currentNode.getNext()
+            previousNode = placeholder
+            i += 1
+
+        previousNode.setNext(currentNode.getNext())
+        self.length -= 1
+
+
+def main():
+    l = LinkedList()
+    for i in range(30):
+        l + i
+    print l
+    for i in range(30):
+        l.pop()
+        print l
+    for i in range(30):
+        l+i
+    for i in range(30):
+        l.removeIndex(-1)
+        print l
+
+if __name__ == '__main__':
+    main()
